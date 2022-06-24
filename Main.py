a3ffdc3098a2758eac650c35245555a2961f44a6
Eviotie <104661931+Eviotie@users.noreply.github.com>
2022-06-24 16:27:47 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from turtle import window_width
 import pytube
 from pyparsing import White
 
@@ -18,13 +19,26 @@ def click():
 window = Tk()
 window.title("Youtube_video_downloader")
 window.configure(background="#2e2c35")
-window.geometry("750x500+400+300")
+#center
+window_width = 150
+window_height = 200
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+window.geometry(f'{window_height}x{window_width}+{center_x}+{center_y}')
+
 Label(window, text="Enter link bellow", bg ="#2e2c35", fg="White", font= "arial 12 bold").grid(row=0, column=0, stick=W)
+#Text Entry box
 textentry = Entry(window, width=20, bg="white")
 textentry.grid(row=1, column=0, sticky=W)
+#dowload button
+
 Button(window, text="Dowload", width = 0, command=click) .grid(row=3, column=0, stick=W)
+#output
+
 output = Text(window, width=20, height=1, wrap=WORD, background="#2e2c35", fg="White", font="arial 12 bold")
 output.grid(row=4, column=0, sticky=W)
 
-
+#mainloop for gui
 window.mainloop()
