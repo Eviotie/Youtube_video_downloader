1675c9ab95e5dd45ff25daa1a984e21015bde244
Eviotie <104661931+Eviotie@users.noreply.github.com>
2022-06-25 13:14:42 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,13 @@
 from tkinter import *
-from turtle import window_width
+from tkinter.ttk import Progressbar
 import pytube
-from pyparsing import White
+import os
+from pathlib import Path
+import time
+from tkinter import ttk
 
+
+downloads_path = str(Path.home() / "Downloads")
 #keydown
 def click():
     link=textentry.get()
@@ -10,7 +15,8 @@ def click():
     try:   
         yt = pytube.YouTube(link)
         ys = yt.streams.get_highest_resolution()
-        ys.download()
+        text = "Dowloading"
+        ys.download(downloads_path)
         text = "Dowloaded"
     except:     
         text = "Not a vaild link"
@@ -21,7 +27,7 @@ window.title("Youtube_video_downloader")
 window.configure(background="#2e2c35")
 
 #center
-window_width = 200
+window_width = 300
 window_height = 400
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
@@ -29,17 +35,20 @@ center_x = int(screen_width/2 - window_width / 2)
 center_y = int(screen_height/2 - window_height / 2)
 window.geometry(f'{window_height}x{window_width}+{center_x}+{center_y}')
 
-Label(window, text="Enter link bellow", bg ="#2e2c35", fg="White", font= "arial 24 bold",).grid(row=0, column=0, stick=W)
+Label(window, text="Enter link bellow", bg ="#2e2c35", fg="White", font= "arial 24 bold",).pack(pady=10, padx=10)
 #Text Entry box
 textentry = Entry(window, width=22, bg="white", font="ariall 24 bold")
-textentry.grid(row=1, column=0, sticky=W)
+textentry.pack(pady=10, padx=10)
 #dowload button
 
-Button(window, text="Dowload", width = 56, command=click).grid(row=3, column=0, stick=W)
-#output
+Button(window, text="Dowload", width = 56, command=click).pack(pady=10, padx=10)
+#progressbar (not functiong)
+'''pb = Progressbar(window, orient="horizontal", length=250, mode="determinate")
+pb.grid(row = 4, column = 0)'''
 
+#output
 output = Text(window, width=44, height=2, wrap=WORD, background="#2e2c35", fg="White", font="arial 12 bold")
-output.grid(row=4, column=0, sticky=W)
+output.pack(pady=10, padx=10)
 
 #mainloop for gui
 window.mainloop()
