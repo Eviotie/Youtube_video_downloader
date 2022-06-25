4857807d9a77fdab05f66d28a236e9924471d30b
Eviotie <104661931+Eviotie@users.noreply.github.com>
2022-06-24 17:02:05 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -19,16 +19,17 @@ def click():
 window = Tk()
 window.title("Youtube_video_downloader")
 window.configure(background="#2e2c35")
+
 #center
-window_width = 150
-window_height = 200
+window_width = 300
+window_height = 400
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 center_x = int(screen_width/2 - window_width / 2)
 center_y = int(screen_height/2 - window_height / 2)
 window.geometry(f'{window_height}x{window_width}+{center_x}+{center_y}')
 
-Label(window, text="Enter link bellow", bg ="#2e2c35", fg="White", font= "arial 12 bold").grid(row=0, column=0, stick=W)
+Label(window, text="Enter link bellow", bg ="#2e2c35", fg="White", font= "arial 24 bold",).grid(row=0, column=0, stick=W)
 #Text Entry box
 textentry = Entry(window, width=20, bg="white")
 textentry.grid(row=1, column=0, sticky=W)
